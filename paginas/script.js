c7481c81c9804078b8ba7532b75c22f141c7757c
Enoc Vargas <edwinfenix11@gmail.com>
2025-12-01 13:37:20 -0600

    Proyecto Revisado

--- paginas/script.js
+++ paginas/script.js
@@ -1,5 +1,5 @@
 // Cargar el header desde header.html
-fetch('header.html')
+fetch('../header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
